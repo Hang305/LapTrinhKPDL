b7d9427c3d489d69c60a6492fa02054473e1798a
Thanh Hang <hanglee305@gmail.com>
2021-03-01 14:19:45 +0700

    thao tác tập tin thư mục P1

---
+++ LapTrinhHDT_Java/src/laptrinhhdt_java/Chuong05/DongLenh/testFileAndDirectory.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laptrinhhdt_java.Chuong05.DongLenh;
+
+import java.io.File;
+import laptrinhhdt_java.Chuong05.Logic.FileAndDirectoryOperation;
+
+/**
+ *
+ * @author Admin
+ */
+public class testFileAndDirectory {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        FileAndDirectoryOperation fo = new FileAndDirectoryOperation();
+//        File[] content = fo.getDirecttoryContent("E:\\KPDL\\BaiTap\\LTHDT_Java\\LapTrinhKPDL\\LapTrinhHDT_Java\\build\\classes\\laptrinhhdt_java");
+////        System.out.print(fo.displayContent(content));
+//        //hiển thị danh sách các thư mục
+//        File[] thumuc = fo.getSubDirectory(content);
+//        System.out.println("Danh sách các thư mục :");
+//        System.out.println(fo.displayContent(thumuc));
+//        //hiển thị danh sách tập tin
+//        System.out.print(fo.displayContent(content));
+//        File[] taptin = fo.getFiles(content);
+//        System.out.println("Danh sách các tập tin :");
+//        System.out.println(fo.displayContent(taptin));
+fo.getContentRecursively("F:\\DAIHOC");
+
+    }
+
+}
---
+++ LapTrinhHDT_Java/src/laptrinhhdt_java/Chuong05/Logic/FileAndDirectoryOperation.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laptrinhhdt_java.Chuong05.Logic;
+
+import java.io.File;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Admin
+ */
+public class FileAndDirectoryOperation {
+
+    public FileAndDirectoryOperation() {
+    }
+    
+    //thao tác liệt kê tất cả các thư mục và tập tin trong thư mục hiện tại
+    public File[] getDirecttoryContent (String folder){
+        File directory = new File(folder);
+        if (directory.isFile()) {
+            return  null;
+        }
+        File[] result = directory.listFiles();
+        return  result;
+    }
+    //thao tác xuất kết quả ra màn hình
+    public String displayContent(File[] content){
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < content.length; i++) {
+            sb.append(content[i].getPath()).append(File.separator).append(content[i].getName());
+            sb.append("\n");
+            System.out.print(sb);
+            
+        }
+        return sb.toString();
+    }
+    //thao tác lọc ra các thư mục
+    public File[] getSubDirectory(File[] content){
+        ArrayList<File> result = new ArrayList<>();
+        for (int i = 0; i < content.length; i++) {
+            if (content[i].isDirectory()) {
+                result.add(content[i]);
+            }
+        }
+        File[] ketqua = new File[result.size()];
+        return result.toArray(ketqua);
+    }
+    
+    //thao tác lọc ra tất cả các tập tin 
+    public File[] getFiles(File[] content){
+        ArrayList<File> result = new ArrayList<>();
+        for (int i = 0; i < content.length; i++) {
+            if (content[i].isFile()) {
+                result.add(content[i]);
+            }
+        }
+        File[] ketqua = new File[result.size()];
+        return result.toArray(ketqua);
+    }
+    
+    //thao tác duyệt đệ quy xuất tất cả các tập tin thư mục
+    public void getContentRecursively (String folder){
+            File content = new File(folder);
+            //điều kiện dừng 
+            if (content.isFile()) {
+                System.out.println(File.separator + content.getPath() +content.getName());
+                return;
+        }
+            //gọi đệ quy trong stack
+            System.out.println("Thư mục "+ folder);
+            File [] sub = content.listFiles();
+            for (int i = 0; i < sub.length; i++) {
+                getContentRecursively(folder +File.separator +sub[i].getName());
+        }
+    }
+}
